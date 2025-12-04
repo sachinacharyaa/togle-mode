c51774a3e34fed8f5b8810b374aea1a8919859e8
sachinacharyaa <sachinacharyahancy@gmail.com>
2025-12-04 21:32:56 +0545

    final

---
+++ twogle.js
@@ -0,0 +1,23 @@
+let modebtn = document.querySelector('.circ');
+let body = document.querySelector("body")
+currentMode = "light";
+
+
+modebtn.addEventListener ("click", () =>{
+  modebtn.classList.toggle("active");
+
+  if (currentMode === "light"){
+     currentMode = "dark";
+       body.classList.remove("light"); // remove old class
+       body.classList.add("dark"); // add new one
+    
+  }else{
+    currentMode = "light";
+     body.classList.remove("dark"); // remove old class
+    body.classList.add("light"); // add new one
+    
+
+  }
+console.log(currentMode);
+});
+
