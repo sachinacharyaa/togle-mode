668a4d4092968310b522504bcd5837d9e5fb4be5
Sachin Acharya <115999060+sachinacharyaa@users.noreply.github.com>
2025-12-04 21:35:57 +0545

    Update twogle.js

--- twogle.js
+++ twogle.js
@@ -21,3 +21,28 @@ modebtn.addEventListener ("click", () =>{
 console.log(currentMode);
 });
 
+
+
+// let modebtn = document.querySelector('.circ');
+// let body = document.querySelector("body")
+// currentMode = "light";
+
+
+// modebtn.addEventListener ("click", () =>{
+//   modebtn.classList.toggle("active");
+
+//   if (currentMode === "light"){
+//      currentMode = "dark";
+//        body.classList.remove("light"); // remove old class
+//        body.classList.add("dark"); // add new one
+    
+//   }else{
+//     currentMode = "light";
+//      body.classList.remove("dark"); // remove old class
+//     body.classList.add("light"); // add new one
+    
+
+//   }
+// console.log(currentMode);
+// });
+
