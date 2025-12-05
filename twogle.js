2c9d6b447cbbfbe04814772fd9353806fef1e826
sachinacharyaa <sachinacharyahancy@gmail.com>
2025-12-05 20:53:21 +0545

    only one
    git push origin main

--- twogle.js
+++ twogle.js
@@ -1,4 +1,4 @@
-let modebtn = document.querySelector('.circ');
+let modebtn = document.querySelector('.fa-sun');
 let body = document.querySelector("body")
 currentMode = "light";
 
@@ -10,39 +10,20 @@ modebtn.addEventListener ("click", () =>{
      currentMode = "dark";
        body.classList.remove("light"); // remove old class
        body.classList.add("dark"); // add new one
+         modebtn.classList.remove("fa-sun");
+        modebtn.classList.add("fa-moon");
     
   }else{
     currentMode = "light";
      body.classList.remove("dark"); // remove old class
     body.classList.add("light"); // add new one
+
+    modebtn.classList.remove("fa-moon");
+        modebtn.classList.add("fa-sun");
     
 
   }
 console.log(currentMode);
 });
 
-
-
-// let modebtn = document.querySelector('.circ');
-// let body = document.querySelector("body")
-// currentMode = "light";
-
-
-// modebtn.addEventListener ("click", () =>{
-//   modebtn.classList.toggle("active");
-
-//   if (currentMode === "light"){
-//      currentMode = "dark";
-//        body.classList.remove("light"); // remove old class
-//        body.classList.add("dark"); // add new one
-    
-//   }else{
-//     currentMode = "light";
-//      body.classList.remove("dark"); // remove old class
-//     body.classList.add("light"); // add new one
-    
-
-//   }
-// console.log(currentMode);
-// });
 
